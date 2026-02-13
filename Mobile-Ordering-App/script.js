e89109dbc73e97913f4f23a7e4b6de3847b7320a
Manika Oberoi <oberoimanika878@gmail.com>
2026-02-13 18:53:36 +0530

    formData, payment btn added

--- Mobile-Ordering-App/script.js
+++ Mobile-Ordering-App/script.js
@@ -1,31 +1,23 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     console.log('DOM loaded!');
-    
-//     const paymentCard = document.getElementById('payment-card');
-//     const completeOrderBtn = document.getElementById('complete-order-btn');
-    
-//     console.log('Payment card:', paymentCard);
-//     console.log('Complete order button:', completeOrderBtn);
-    
-//     if (completeOrderBtn) {
-//         completeOrderBtn.addEventListener('click', function() {
-//             console.log('Button clicked!');
-//             paymentCard.style.display = 'flex';
-//         });
-//     } else {
-//         console.log('Button not found!');
-//     }
-// });
-
-
-// document.addEventListener("DOMContentLoaded", function() {
-    
-// })
-
-
 const paymentCard = document.getElementById('payment-card')
 const completeOrderBtn = document.getElementById('complete-order-btn')
+const paymentBtn = document.getElementById('payment-btn')
+const forRtNow = document.getElementById('load')
+const paymentForm = document.getElementById('payment-form')
 
 completeOrderBtn.addEventListener('click', function() {
     paymentCard.style.display = 'flex'
+})
+
+paymentForm.addEventListener('submit', function(e) {
+    e.preventDefault()
+
+    const paymentFormData = new FormData(paymentForm)
+    const fullName = paymentFormData.get('fullName')
+
+    paymentCard.style.display = 'none'
+
+    forRtNow.innerHTML = `
+    <div id="thanks-box">
+        <p id="thanks-text">Thanks, ${fullName}! Your order is on its way!</p>
+    </div>`
 })
