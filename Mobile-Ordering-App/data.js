1623d735cbeb76d10de279a1924b874ecffefc15
Manika Oberoi <oberoimanika878@gmail.com>
2026-02-17 15:01:19 +0530

    added data.js file for mobile-ordering app and rewrite code for js

---
+++ Mobile-Ordering-App/data.js
@@ -0,0 +1,23 @@
+export const menuArray = [
+    {
+        name: "Pizza",
+        ingredients: ["pepperoni", "mushrom", "mozarella"],
+        id: 0,
+        price: 14,
+        emoji: "Images/pizza-svg.svg"
+    },
+    {
+        name: "Hamburger",
+        ingredients: ["corns", "cheese", "lettuce"],
+        price: 12,
+        emoji: 'Images/hamburger-svg.svg',
+        id: 1
+    },
+        {
+        name: "Beer",
+        ingredients: ["grain, hops, yeast, water"],
+        price: 12,
+        emoji: "Images/beer-svg.svg",
+        id: 2
+    }
+]
--- Mobile-Ordering-App/script.js
+++ Mobile-Ordering-App/script.js
@@ -1,8 +1,44 @@
+import { menuArray } from './data.js'
+
 const paymentCard = document.getElementById('payment-card')
 const completeOrderBtn = document.getElementById('complete-order-btn')
 const paymentBtn = document.getElementById('payment-btn')
 const forRtNow = document.getElementById('load')
 const paymentForm = document.getElementById('payment-form')
+
+function getFeedHtml(){
+    let feedHtml = ``
+    
+    menuArray.forEach(function(item){
+        feedHtml += `
+        <section class="order-items">
+                <article class="food-container">
+                    <section class="left">
+                        <img src="${item.emoji}" class="food-img">
+                        <div class="info-food">
+                            <h3 class="food-name">${item.name}</h3>
+                            <p class="food-desc">${item.ingredients}</p>
+                            <h4 class="food-price">$${item.price}</h4>
+                        </div>
+                    </section>
+
+                    <section class="right">
+                        <button class="add-btn" aria-label="the button represents add label">+</button>
+                    </section>
+                </article>
+
+                <div class="divider"></div>
+            `
+    })
+
+    return feedHtml
+}
+
+function render(){
+    document.getElementById('feed').innerHTML = getFeedHtml()
+}
+
+render()
 
 completeOrderBtn.addEventListener('click', function() {
     paymentCard.style.display = 'flex'
